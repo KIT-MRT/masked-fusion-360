3b12e871fe74fe301ed3d64c31fa2ca0c1c042a4
roydenwa <royden-wagner@outlook.com>
2023-09-15 09:44:14 +0200

    Remove unused code

--- src/masked_fusion_360/ros_utils/ros_opencv_utils.py
+++ src/masked_fusion_360/ros_utils/ros_opencv_utils.py
@@ -19,26 +19,10 @@ def imgmsg_to_cv2(img_msg):
     return image_opencv
 
 
-def imgmsg_to_cv2_f32(img_msg):
-    print("encoding: ", img_msg.encoding)
-    print("height: ", img_msg.height)
-    print("width: ", img_msg.width)
-    dtype = np.dtype("float32")
-    dtype = dtype.newbyteorder('>' if img_msg.is_bigendian else '<')
-    image_opencv = np.ndarray(
-        shape=(img_msg.height, img_msg.width, 1),
-        dtype=dtype,
-        buffer=img_msg.data,
-    )
-    if img_msg.is_bigendian == (sys.byteorder == 'little'):
-        image_opencv = image_opencv.byteswap().newbyteorder()
-    
-    return image_opencv
-
 def f32c1_immsg_to_nparray(img_msg):
-    dtype = np.dtype("uint8") # Hardcode to 8 bits...
+    dtype = np.dtype("uint8") 
     dtype = dtype.newbyteorder('>' if img_msg.is_bigendian else '<')
-    image_opencv = np.ndarray(shape=(img_msg.height * img_msg.width * 4), # and three channels of data. Since OpenCV works with bgr natively, we don't need to reorder the channels.
+    image_opencv = np.ndarray(shape=(img_msg.height * img_msg.width * 4), 
                     dtype=dtype, buffer=img_msg.data)
     # If the byt order is different between the message and the system.
     if img_msg.is_bigendian == (sys.byteorder == 'little'):
@@ -56,14 +40,6 @@ def f32c1_immsg_to_nparray(img_msg):
 
     return image_opencv
 
-
-
-def f32c1_imgmsg_to_opencv(img_msg, bridge):
-    cv_img = bridge.imgmsg_to_cv2(img_msg, "32FC1")
-    cv_img_array = np.array(cv_img, dtype=np.dtype("f8"))
-
-    return cv_img_array
-    
 
 
 def cv2_to_imgmsg(cv_image):
