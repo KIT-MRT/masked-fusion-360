a04143fdeedd18c99e0d8cda8a016655d17c85b6
roydenwa <royden-wagner@outlook.com>
2023-09-19 13:18:47 +0200

    Format code

--- src/masked_fusion_360/ros_utils/ros_opencv_utils.py
+++ src/masked_fusion_360/ros_utils/ros_opencv_utils.py
@@ -8,26 +8,36 @@ from sensor_msgs.msg import Image
 
 def imgmsg_to_cv2(img_msg):
     if img_msg.encoding != "bgr8":
-        rospy.logerr("This Coral detect node has been hardcoded to the 'bgr8' encoding.  Come change the code if you're actually trying to implement a new camera")
-    dtype = np.dtype("uint8") # Hardcode to 8 bits...
-    dtype = dtype.newbyteorder('>' if img_msg.is_bigendian else '<')
-    image_opencv = np.ndarray(shape=(img_msg.height, img_msg.width, 3), # and three channels of data. Since OpenCV works with bgr natively, we don't need to reorder the channels.
-                    dtype=dtype, buffer=img_msg.data)
-    # If the byt order is different between the message and the system.
-    if img_msg.is_bigendian == (sys.byteorder == 'little'):
+        rospy.logerr(
+            "This Coral detect node has been hardcoded to the 'bgr8' encoding.  Come change the code if you're actually trying to implement a new camera"
+        )
+    dtype = np.dtype("uint8")
+    dtype = dtype.newbyteorder(">" if img_msg.is_bigendian else "<")
+    image_opencv = np.ndarray(
+        shape=(
+            img_msg.height,
+            img_msg.width,
+            3,
+        ),
+        dtype=dtype,
+        buffer=img_msg.data,
+    )
+    # If the byte order is different between the message and the system
+    if img_msg.is_bigendian == (sys.byteorder == "little"):
         image_opencv = image_opencv.byteswap().newbyteorder()
     return image_opencv
 
 
 def f32c1_imgmsg_to_nparray(img_msg):
-    dtype = np.dtype("uint8") 
-    dtype = dtype.newbyteorder('>' if img_msg.is_bigendian else '<')
-    image_opencv = np.ndarray(shape=(img_msg.height * img_msg.width * 4), 
-                    dtype=dtype, buffer=img_msg.data)
+    dtype = np.dtype("uint8")
+    dtype = dtype.newbyteorder(">" if img_msg.is_bigendian else "<")
+    image_opencv = np.ndarray(
+        shape=(img_msg.height * img_msg.width * 4), dtype=dtype, buffer=img_msg.data
+    )
     # If the byt order is different between the message and the system.
-    if img_msg.is_bigendian == (sys.byteorder == 'little'):
+    if img_msg.is_bigendian == (sys.byteorder == "little"):
         image_opencv = image_opencv.byteswap().newbyteorder()
-    
+
     image_opencv = image_opencv.view(np.float32)
 
     image_opencv = np.nan_to_num(image_opencv, nan=0.0)
@@ -57,11 +67,11 @@ def cv2_to_imgmsg(cv_image):
         img_msg.encoding = "bgr8"
     else:
         img_msg.encoding = "mono8"
-    
+
     img_msg.is_bigendian = 0
     img_msg.data = cv_image.tostring()
     img_msg.step = len(img_msg.data) // img_msg.height
-    
+
     return img_msg
 
 
